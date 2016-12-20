e2083dced19563591ccb5edf04df1d1054052c63
Julian Kyer <julian.kyer@gmail.com>
2016-12-20 14:23:12 -0700

    Add responsive card Vertical height needs adjusting for long cards

--- script.js
+++ script.js
@@ -8,20 +8,14 @@ $('.save-button').click(function(event) {
   clearIdeaInputs();
   });
 
-
 function displayIdea ($ideaTitle, $ideaBody) {
-  $('.idea-section').prepend(`  <div class ="idea-render">
-  <div class = "title-content">
-  <h2 class ="title-render" contenteditable="true">${$ideaTitle}</h2>
-  <button class="button delete">delete</button>
-  <p class = "editable-body" contenteditable="true" >${$ideaBody}</p>
-  <p>
-  <button class = "button upvote"></button>
-  <button class = "button downvote"></button>
-  <span class="quality-text">quality: swill</span>
-  </p>
-  <hr>
-  </div>
+  $('.idea-section').prepend(`<div class ="idea-render">
+    <h2 class ="title-render" contenteditable="true">${$ideaTitle}</h2>
+    <button class="button delete">delete</button>
+    <p class = "editable-body" contenteditable="true" >${$ideaBody}</p>
+    <button class = "button upvote"></button>
+    <button class = "button downvote"></button>
+    <span class="quality-text">quality: swill</span>
   </div>`);
 }
 
