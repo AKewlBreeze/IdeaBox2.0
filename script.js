f94da0b496325f43b30c7f46b002cc989200db4f
Breana Knight <Akewlbreeze.bc@gmail.com>
2016-12-26 14:58:46 -0700

    fixes $ misplacement for quality

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ $('.idea-section').on('click', '.upvote', function() {
 
 $('.idea-section').on('click', '.downvote', function() {
   var $targetID = $(this).closest('.idea-render').attr('id');
-  var targetIdea = JSON.parse(localStorage.getItem(target$ID));
+  var targetIdea = JSON.parse(localStorage.getItem($targetID));
   if (targetIdea.quality === 'genius'){
     targetIdea.quality = 'plausible';
   } else if (targetIdea.quality === 'plausible'){
